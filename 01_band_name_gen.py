b5c8daf337c4b9deeb5681af7f929c6e6ee84bed
rinat iamaev <rinatiamaev@gmail.com>
2024-12-30 00:30:46 +0200

    day 01

---
+++ 01_band_name_gen.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator.")
+city = input("What's your favorite city?\n")
+best_friend = input("What's your best friend's name?\n")
+print("Your band name could be " + city + " " + best_friend)
