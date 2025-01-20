cc3d689d14d745e0da50f4e3d8a4e2f471c754e3
rinat iamaev <rinatiamaev@gmail.com>
2025-01-20 23:36:29 +0200

    day 20 - 21 snack game

--- 20_snackGame.py
+++ 20_snackGame.py
@@ -1,6 +1,6 @@
 from turtle import Turtle, Screen
-import time
-start_pos = [(0,0),(-20,0),(-40,0),]
+import time, random
+START_POS = [(0,0),(-20,0),(-40,0),]
 UP = 90
 DOWN = 270
 RIGHT = 0
@@ -20,14 +20,20 @@ class Snack:
         self.head = self.segments[0]
 
     def create_snack(self):
-        
-        for pos in start_pos:
-            seg = Turtle()
-            seg.shape("square")
-            seg.color("white")
-            seg.penup()
-            seg.goto(pos)
-            self.segments.append(seg)
+        for pos in START_POS:
+            self.add_seg(pos)
+
+    def add_seg(self, position):
+        seg = Turtle()
+        seg.shape("square")
+        seg.color("white")
+        seg.penup()
+        seg.goto(position)
+        self.segments.append(seg)
+    
+    def extend_snack(self):
+        self.add_seg(self.segments[-1].position())
+
 
 
     def move(self):
@@ -55,16 +61,90 @@ class Snack:
 
 
 
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("pink")
+        self.speed("fastest")
+        self.refresh()
+    
+    def refresh(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
+
 my_snack = Snack()
+food = Food()
+
 screen.listen()
 screen.onkey(my_snack.up, "w")
 screen.onkey(my_snack.down, "s")
 screen.onkey(my_snack.left, "a")
 screen.onkey(my_snack.right, "d")
 game_on = True
+
+score_turtle = Turtle()
+score_turtle.hideturtle()
+score_turtle.color("white")
+score_turtle.penup()
+score_turtle.goto(-280, 260)  
+
+class PointsCount(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("yellow")
+        self.penup()
+        self.goto(0, 270)
+        self.score = 0
+        self.clear()
+        self.update_screen()
+        self.hideturtle()
+
+    def update_screen(self):
+        self.write(f"Points = {self.score}", align="center", font=("Arial", 16, "bold"))
+
+    def increment_score(self):
+        self.score += 1
+        self.clear()
+        self.update_screen()
+
+class GameOver(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("yellow")
+        self.penup()
+        self.hideturtle()
+    
+    def print_msg(self):
+        self.write("GAME OVER.", align="center", font=("Arial", 16, "bold"))
+
+
+score = PointsCount()
+game_over = GameOver()
 while game_on:
     screen.update()
     time.sleep(0.1)
     my_snack.move()
 
+    if my_snack.head.distance(food) < 15:
+        food.refresh()
+        my_snack.extend_snack()
+        score.increment_score()
+        
+    
+    if my_snack.head.xcor() > 280 or my_snack.head.xcor() < -280 or my_snack.head.ycor() > 280 or my_snack.head.ycor() < -280:
+        game_over.print_msg()
+        game_on = False
+        
+    for seg in my_snack.segments[1:]:
+        if my_snack.head.distance(seg) < 10:
+            game_on = False
+            game_over.print_msg()
+
 screen.exitonclick()
+
+
