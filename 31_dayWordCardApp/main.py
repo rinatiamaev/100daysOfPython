c414976f489e40cf330723f8dba8eb670637dba7
rinat iamaev <rinatiamaev@gmail.com>
2025-02-09 21:50:04 +0200

    day 31 WordCard App

---
+++ 31_dayWordCardApp/main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("WordCardApp")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Ariel", 65, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+button_wrong = Button(image=wrong_image, highlightthickness=0)
+button_wrong.grid(row=1, column=0)
+
+right_image = PhotoImage(file="images/right.png")
+button_right = Button(image=right_image, highlightthickness=0)
+button_right.grid(row=1, column=1)
+
+window.mainloop()
